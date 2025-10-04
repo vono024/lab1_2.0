b1c198a7b009acd38f25ef0909b03c38fed2eda5
vono <vani4kamaksimov.2006@gmail.com>
2025-10-04 18:18:51 +0300

    +++

--- lab1/routes/web.php
+++ lab1/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LabController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['query.mode'])->group(function () {
+    Route::get('/', [LabController::class, 'index'])->name('lab.index');
+    Route::get('/about', [LabController::class, 'about'])->name('lab.about');
+    Route::get('/status', [LabController::class, 'status'])->name('lab.status');
+    Route::get('/echo', [LabController::class, 'echo'])->name('lab.echo');
 });
